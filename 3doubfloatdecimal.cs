29948cc4fff5fcdb9b4d2fd9ce3d083bfb8864c5
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-03-04 11:10:14 +0530

    Add files via upload

---
+++ 3doubfloatdecimal.cs
@@ -0,0 +1,21 @@
+// doubfloatdecimal
+using System; 
+namespace FloatingPointNumbers{
+public class program 
+{
+	public  static void Main(string[] args)
+	{
+		float f_no = 0.0f;
+		double d_no=0.0d;
+		decimal m_no=0.0m;
+		f_no=1/3f;
+		d_no=1/3d;
+		m_no=1/3m;
+		Console.WriteLine(f_no);
+		Console.WriteLine(d_no);
+		Console.WriteLine(m_no);
+
+	}
+
+}
+}
