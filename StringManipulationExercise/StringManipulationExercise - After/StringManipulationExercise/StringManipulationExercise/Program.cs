bfe46e4eae81fbdb9294e5b5c218bcd607bedfba
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-25 21:28:37 +0530

    Add files via upload

---
+++ StringManipulationExercise/StringManipulationExercise - After/StringManipulationExercise/StringManipulationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvShow = "Game of Thrones    ";
+            string seasonOne = " - Season 1";
+            string newName = "";
+
+            // Add " - Season 1" 
+            // without you determining the start index 
+
+            newName = tvShow.Insert(tvShow.Trim().Length, seasonOne);
+            Console.WriteLine(newName);
+        }
+    }
+}
