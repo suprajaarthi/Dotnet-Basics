4b4f67828fd26b1986388308f1bce4fbf8e9d972
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-26 00:50:11 +0530

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double gravityForce = 9.8;
+            double PI = 3.14159265359;
+            
+
+            Console.WriteLine("Gravity force is " + gravityForce + " and PI is " + PI);
+        }
+    }
+}
---
+++ EscapeSequences.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeSequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" And Jon said \"Bend \t \t \n \a the knee \" ");
+            Console.WriteLine("D:\\Movies");
+        }
+    }
+}
