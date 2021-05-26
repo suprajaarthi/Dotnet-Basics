8063a40c129f51294ab41b03afc04e191917b18a
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-26 12:05:52 +0530

    Add files via upload

---
+++ CryptographyApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CryptographyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DESCryptoServiceProvider key = new DESCryptoServiceProvider();
+            Console.WriteLine("Enter your message: ");
+            string plainText = Console.ReadLine();
+
+            string cipherText = Encrypt(plainText, key);
+
+            Console.WriteLine("\nCipher Text: " + Encrypt(plainText,key));
+            Console.WriteLine("Plain Text: " + Decrypt(cipherText, key));
+        }
+
+        public static string Encrypt(string plainText, SymmetricAlgorithm key)
+        {
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, key.CreateEncryptor(), CryptoStreamMode.Write);
+            StreamWriter sw = new StreamWriter(cs);
+            sw.Write(plainText);
+            sw.Flush();
+            cs.FlushFinalBlock();
+            return (Convert.ToBase64String(ms.GetBuffer(),0,(int)ms.Length));
+        }
+
+        public static string Decrypt(string cipherText, SymmetricAlgorithm key)
+        {
+            MemoryStream ms = new MemoryStream(Convert.FromBase64String(cipherText));
+            CryptoStream cs = new CryptoStream(ms, key.CreateDecryptor(), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
+
+    }
+}
---
+++ DefaultKeyword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long longVar = default;
+            string stringVar = default;
+            DateTime dateVar = default;
+            int intVar = default;
+
+            Console.WriteLine(dateVar);
+        }
+    }
+}
