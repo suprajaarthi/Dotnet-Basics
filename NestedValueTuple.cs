c8528483a68f2852816af43c67cc68e7ca7a94a8
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-26 12:28:27 +0530

    Chap 7 Generic & Non-Generic

---
+++ NestedValueTuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedValueTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ronaldoGoals = (Year2017:64,Year2016:60,Year2015:50,Year2014:55);
+            var ronaldo = (playerNo: 7, playerName: "Ronaldo", playerGoals:ronaldoGoals);
+
+            Console.WriteLine(ronaldo.playerNo);
+            Console.WriteLine(ronaldo.playerName);
+            Console.WriteLine(ronaldo.playerGoals.Year2017);
+            Console.WriteLine(ronaldo.playerGoals.Year2016);
+            Console.WriteLine(ronaldo.playerGoals.Year2015);
+            Console.WriteLine(ronaldo.playerGoals.Year2014);
+        }
+    }
+}
---
+++ ValueTupleswithMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTuplesWithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ronaldo = (7, "Ronaldo", 60);
+            var giggs = (10, "Giggs", 40);
+
+            DisplayPlayerInformation(ronaldo);
+            DisplayPlayerInformation(giggs);
+
+            DisplayPlayerInformation(DisplayPlayerInformation());
+        }
+
+        public static void DisplayPlayerInformation((int playerNo, string playerName, int playerGoal) player)
+        {
+            Console.WriteLine(player.playerNo);
+            Console.WriteLine(player.playerName);
+            Console.WriteLine(player.playerGoal);
+            Console.WriteLine("-------------------");
+        }
+
+        public static (int, string, int) DisplayPlayerInformation()
+        {
+            var messi = (10, "Messi", 60);
+            return messi;
+        }
+    }
+}
