112c65a3ec77b06f59647b5c7475e74cc97cfa3f
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-26 10:50:43 +0530

    OOPS Part 2

---
+++ Enumeration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weekDayName = WeekDay.Monday.ToString();
+            WeekDay day = WeekDay.Sunday;
+
+            Console.WriteLine((int)day);
+        }
+
+    }
+
+    enum WeekDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday = 40,
+        Saturday = 50,
+        Sunday = 60
+    }
+}
