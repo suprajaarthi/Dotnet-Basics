c5702a6936a4ade2dd62e0b7a7573b68f670dfa4
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 21:27:38 +0530

    FreeCodeCamp

---
+++ GetUserInput.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name ");
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.Write("Hello " + age);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+//Enter your name Supraja
+//Hello Supraja 
