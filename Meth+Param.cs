9ef228d3c98353ee83049a79c99baeeae30f53f6
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 23:50:41 +0530

    Freecodecamp Methods accessing parameters

---
+++ Meth+Param.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayhi("Cat");
+            Console.ReadLine();
+
+        }
+        static void Sayhi(string name)
+        {
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
+
+//Hello user
+
