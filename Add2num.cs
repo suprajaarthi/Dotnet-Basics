29a54fbaa4957afb07eb533afa47d8c39f18bb5f
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 21:34:01 +0530

    FreeCodeCamp

---
+++ Add2num.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1+num2);
+           
+        }
+    }
+}
+
+//Enter your name Supraja
+//Hello Supraja 
