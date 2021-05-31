640917f44ef7166dcb788959850f23a90a71e684
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-01 00:20:40 +0530

    Freecodecamp if conditionals

---
+++ If.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            if(isMale)
+            {
+                Console.WriteLine("You are male");
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
+
+//125
