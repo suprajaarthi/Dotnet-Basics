e2b1b8363bd94c09385d4a6b8b6f946de1362095
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-01 00:42:05 +0530

    Complete calculator FreeCodeCamp

---
+++ BasicCalculator.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator : ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
