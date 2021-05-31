8e45053f3237e845bb0942b235e071a26887281a
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 23:49:24 +0530

    Create Methods.cs

---
+++ Methods.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sayhi();
+            Console.ReadLine();
+
+        }
+        static void Sayhi()
+        {
+            Console.WriteLine("Hello user");
+        }
+    }
+}
+
+//Hello user
+// W/O calling the sayHi() function in main , it will never get executed
