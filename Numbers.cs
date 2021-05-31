62be5109cb5ba891c174ba048361991c28e53fc4
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 21:24:25 +0530

    Freecodecamp

---
+++ Numbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(3,2));
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Min(4, 90));
+            Console.WriteLine(Math.Round(4.6));
+
+
+
+
+        }
+    }
+}
+
+//90
+//4
+//5
