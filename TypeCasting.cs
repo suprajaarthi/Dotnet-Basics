b7d1aec9f9c5ed406362c1b86aaf75d164067d68
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-31 21:31:16 +0530

    FreeCodeCamp

---
+++ TypeCasting.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace basicsYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("56");
+            Console.Write(num + 6);
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
+
+//Enter your name Supraja
+//Hello Supraja 
