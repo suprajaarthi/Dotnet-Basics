1132002e5cb3fbc6f055929e46216ca8b98ec459
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-01 20:50:00 +0530

    Create For.cs

---
+++ For.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <= 5;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+1
+2
+3
+4
+5
