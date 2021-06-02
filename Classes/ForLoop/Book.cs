d064ee3af672112b25c0c319a3faa07d279fdce7
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-02 09:25:36 +0530

    Update Book.cs

--- Classes/ForLoop/Book.cs
+++ Classes/ForLoop/Book.cs
@@ -9,3 +9,24 @@ namespace ForLoop
         public int pages;
     }
 }
+
+//other way 
+
+using System;
+
+namespace ForLoop
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    
+        public Book(string atitle,string aauthor,string apages)
+        {
+            title=atitle;
+            author=aauthor;
+            pages=apages;
+        }
+    }
+}
