7875b16dd69b24673a26281065ad28980f30adb4
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-02 08:22:51 +0530

    2d Array Freecode camp

---
+++ Matrix.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] numberGrid =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+            Console.WriteLine(numberGrid[1,1]);
+        }
+
+    }
+}
