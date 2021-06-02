7e80663ef18684327e399c21152212e6333be704
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-01 20:35:06 -0700

    Classes and Constructors C#

---
+++ Classes/ForLoop/Book.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ForLoop
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Classes/ForLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            
+            book1.title="Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages=400;
+            Console.WriteLine(book1.author);
+            Console.ReadLine();
+
+
+           
+        }
+    }
+}
+//OP:
+
+//Enter a number :7
+//Enter another number : 0
+//Attempted to divide by 0 
